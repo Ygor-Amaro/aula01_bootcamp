b1bc80bb44cd6c6f7083f32840421a351acc1292
Ygor Amaro <ykba09@gmail.com>
2025-03-13 01:22:10 -0300

    exercicios 1 e 2 do bootcamp de python, jornada de dados

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-#teste
+#Exercício 01 - Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(len(input("digite o seu nome: ")))
+
+#Exercício 02 - Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite o primeiro número: ")) + int(input("Digite o segundo número: ")))
